bdd300b3ba5498b0e73bbab00df760ec4403885f
Bernaldo Napitupulu <bernaldo.stis@gmail.com>
2026-06-20 15:10:15 +0800

    Fix DOMDocument parse error in Excel exports by wrapping in proper HTML tags

---
+++ resources/views/laporan/export/rekap-mingguan-excel.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+    <table>
+        <tbody>
+            <tr>
+                <td colspan="8" style="font-weight: bold; text-align: center; font-size: 16pt;">Laporan Rekap Mingguan</td>
+            </tr>
+            <tr>
+                <td colspan="8" style="text-align: center;">Periode: Minggu 2 (8-14 Juni 2026)</td>
+            </tr>
+            <tr>
+                <td colspan="8"></td>
+            </tr>
+            <tr>
+                <td colspan="8" style="font-weight: bold;">1. Rekap Absensi</td>
+            </tr>
+            <tr>
+                <th style="background-color: #f2f2f2; font-weight: bold; text-align: center; border: 1px solid #000;">Nama</th>
+                <th style="background-color: #f2f2f2; font-weight: bold; text-align: center; border: 1px solid #000;">Sen</th>
+                <th style="background-color: #f2f2f2; font-weight: bold; text-align: center; border: 1px solid #000;">Sel</th>
+                <th style="background-color: #f2f2f2; font-weight: bold; text-align: center; border: 1px solid #000;">Rab</th>
+                <th style="background-color: #f2f2f2; font-weight: bold; text-align: center; border: 1px solid #000;">Kam</th>
+                <th style="background-color: #f2f2f2; font-weight: bold; text-align: center; border: 1px solid #000;">Jum</th>
+                <th style="background-color: #f2f2f2; font-weight: bold; text-align: center; border: 1px solid #000;">Sab</th>
+                <th style="background-color: #f2f2f2; font-weight: bold; text-align: center; border: 1px solid #000;">Total</th>
+            </tr>
+            <tr><td style="border: 1px solid #000;">Ahmad Hidayat</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; font-weight: bold; border: 1px solid #000;">6</td></tr>
+            <tr><td style="border: 1px solid #000;">Siti Aminah</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; border: 1px solid #000;">Izin</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; font-weight: bold; border: 1px solid #000;">5</td></tr>
+            <tr><td style="border: 1px solid #000;">Budi Santoso</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; font-weight: bold; border: 1px solid #000;">6</td></tr>
+            <tr><td style="border: 1px solid #000;">Rohman</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; border: 1px solid #000;">Sakit</td><td style="text-align: center; border: 1px solid #000;">Sakit</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; font-weight: bold; border: 1px solid #000;">4</td></tr>
+            <tr><td style="border: 1px solid #000;">Dewi Lestari</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; border: 1px solid #000;">Alpha</td><td style="text-align: center; font-weight: bold; border: 1px solid #000;">5</td></tr>
+            <tr><td style="border: 1px solid #000;">Ujang Supriatna</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; border: 1px solid #000;">Hadir</td><td style="text-align: center; font-weight: bold; border: 1px solid #000;">6</td></tr>
+            <tr>
+                <td style="font-weight: bold; border: 1px solid #000;">Total</td>
+                <td style="text-align: center; font-weight: bold; border: 1px solid #000;">6</td><td style="text-align: center; font-weight: bold; border: 1px solid #000;">5</td><td style="text-align: center; font-weight: bold; border: 1px solid #000;">4</td><td style="text-align: center; font-weight: bold; border: 1px solid #000;">6</td><td style="text-align: center; font-weight: bold; border: 1px solid #000;">6</td><td style="text-align: center; font-weight: bold; border: 1px solid #000;">5</td><td style="text-align: center; font-weight: bold; border: 1px solid #000;">32</td>
+            </tr>
+            <tr>
+                <td colspan="8"></td>
+            </tr>
+            
+            <tr>
+                <td colspan="8" style="font-weight: bold;">2. Rekap Hasil Kupas</td>
+            </tr>
+            <tr>
+                <th style="font-weight: bold; border: 1px solid #000; background-color: #f2f2f2;">Nama</th>
+                <th style="font-weight: bold; border: 1px solid #000; background-color: #f2f2f2;">Jenis Kupas</th>
+                <th style="font-weight: bold; border: 1px solid #000; background-color: #f2f2f2;">Jumlah</th>
+                <th style="font-weight: bold; border: 1px solid #000; background-color: #f2f2f2;">Tarif</th>
+                <th style="font-weight: bold; border: 1px solid #000; background-color: #f2f2f2;">Total Upah</th>
+            </tr>
+            <tr><td style="border: 1px solid #000;">Siti Aminah</td><td style="border: 1px solid #000;">Kupas Sabut</td><td style="text-align: right; border: 1px solid #000;">850</td><td style="text-align: right; border: 1px solid #000;">500</td><td style="text-align: right; font-weight: bold; border: 1px solid #000;">425000</td></tr>
+            <tr><td style="border: 1px solid #000;">Rohman</td><td style="border: 1px solid #000;">Kupas Sabut</td><td style="text-align: right; border: 1px solid #000;">720</td><td style="text-align: right; border: 1px solid #000;">500</td><td style="text-align: right; font-weight: bold; border: 1px solid #000;">360000</td></tr>
+            <tr><td style="border: 1px solid #000;">Dewi Lestari</td><td style="border: 1px solid #000;">Kupas Tempurung</td><td style="text-align: right; border: 1px solid #000;">600</td><td style="text-align: right; border: 1px solid #000;">800</td><td style="text-align: right; font-weight: bold; border: 1px solid #000;">480000</td></tr>
+            <tr><td style="border: 1px solid #000;">Ujang Supriatna</td><td style="border: 1px solid #000;">Kupas Sabut</td><td style="text-align: right; border: 1px solid #000;">680</td><td style="text-align: right; border: 1px solid #000;">500</td><td style="text-align: right; font-weight: bold; border: 1px solid #000;">340000</td></tr>
+            <tr><td style="border: 1px solid #000;">Ahmad Hidayat</td><td style="border: 1px solid #000;">Kupas Tempurung</td><td style="text-align: right; border: 1px solid #000;">400</td><td style="text-align: right; border: 1px solid #000;">800</td><td style="text-align: right; font-weight: bold; border: 1px solid #000;">320000</td></tr>
+            <tr>
+                <td colspan="2" style="font-weight: bold; border: 1px solid #000;">Total</td>
+                <td style="text-align: right; font-weight: bold; border: 1px solid #000;">3250</td>
+                <td style="border: 1px solid #000;"></td>
+                <td style="text-align: right; font-weight: bold; border: 1px solid #000;">1925000</td>
+            </tr>
+            <tr>
+                <td colspan="8"></td>
+            </tr>
+
+            <tr>
+                <td colspan="8" style="font-weight: bold;">3. Rekap Panen</td>
+            </tr>
+            <tr>
+                <th style="font-weight: bold; border: 1px solid #000; background-color: #f2f2f2;">Tanggal</th>
+                <th style="font-weight: bold; border: 1px solid #000; background-color: #f2f2f2;">Kebun</th>
+                <th style="font-weight: bold; border: 1px solid #000; background-color: #f2f2f2;">Komoditas</th>
+                <th style="font-weight: bold; border: 1px solid #000; background-color: #f2f2f2;">Jumlah</th>
+                <th style="font-weight: bold; border: 1px solid #000; background-color: #f2f2f2;">Nilai</th>
+            </tr>
+            <tr><td style="border: 1px solid #000;">08 Jun 2026</td><td style="border: 1px solid #000;">Kebun Cikaret</td><td style="border: 1px solid #000;">Kelapa Butiran</td><td style="text-align: right; border: 1px solid #000;">450</td><td style="text-align: right; font-weight: bold; border: 1px solid #000;">6750000</td></tr>
+            <tr><td style="border: 1px solid #000;">09 Jun 2026</td><td style="border: 1px solid #000;">Kebun Sukamaju</td><td style="border: 1px solid #000;">Kopra</td><td style="text-align: right; border: 1px solid #000;">120</td><td style="text-align: right; font-weight: bold; border: 1px solid #000;">2880000</td></tr>
+            <tr><td style="border: 1px solid #000;">10 Jun 2026</td><td style="border: 1px solid #000;">Kebun Ciomas</td><td style="border: 1px solid #000;">Kelapa Muda</td><td style="text-align: right; border: 1px solid #000;">300</td><td style="text-align: right; font-weight: bold; border: 1px solid #000;">6000000</td></tr>
+            <tr><td style="border: 1px solid #000;">12 Jun 2026</td><td style="border: 1px solid #000;">Kebun Cikaret</td><td style="border: 1px solid #000;">Kelapa Butiran</td><td style="text-align: right; border: 1px solid #000;">380</td><td style="text-align: right; font-weight: bold; border: 1px solid #000;">5700000</td></tr>
+            <tr><td style="border: 1px solid #000;">14 Jun 2026</td><td style="border: 1px solid #000;">Kebun Sukamaju</td><td style="border: 1px solid #000;">Kopra</td><td style="text-align: right; border: 1px solid #000;">150</td><td style="text-align: right; font-weight: bold; border: 1px solid #000;">3600000</td></tr>
+            <tr>
+                <td colspan="3" style="font-weight: bold; border: 1px solid #000;">Total</td>
+                <td style="text-align: right; font-weight: bold; border: 1px solid #000;">1800</td>
+                <td style="text-align: right; font-weight: bold; border: 1px solid #000;">24930000</td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
---
+++ resources/views/pengajuan/export-excel.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+    <table>
+        <tbody>
+            <tr>
+                <td colspan="5" style="font-weight: bold; font-size: 16px;">PT. TRI MUSTIKA COCOMINAESA</td>
+            </tr>
+            <tr>
+                <td colspan="5">Form Pengajuan Barang & Keperluan</td>
+            </tr>
+            <tr>
+                <td colspan="5"></td>
+            </tr>
+            <tr>
+                <td colspan="2">Tanggal</td>
+                <td colspan="3">: {{ \Carbon\Carbon::parse($pengajuan->tanggal)->format('d F Y') }}</td>
+            </tr>
+            <tr>
+                <td colspan="2">Keperluan</td>
+                <td colspan="3">: {{ $pengajuan->judul_pengajuan }}</td>
+            </tr>
+            <tr>
+                <td colspan="2">Status</td>
+                <td colspan="3">: {{ $pengajuan->status }}</td>
+            </tr>
+            <tr>
+                <td colspan="5"></td>
+            </tr>
+            @if($pengajuan->keterangan)
+            <tr>
+                <td colspan="5" style="font-weight: bold;">Catatan Tambahan:</td>
+            </tr>
+            <tr>
+                <td colspan="5">{{ $pengajuan->keterangan }}</td>
+            </tr>
+            <tr>
+                <td colspan="5"></td>
+            </tr>
+            @endif
+            
+            <tr>
+                <th style="font-weight: bold; background-color: #3B6653; color: #ffffff; text-align: center; border: 1px solid #000000;">No</th>
+                <th style="font-weight: bold; background-color: #3B6653; color: #ffffff; text-align: center; border: 1px solid #000000;">Nama Barang / Deskripsi</th>
+                <th style="font-weight: bold; background-color: #3B6653; color: #ffffff; text-align: center; border: 1px solid #000000;">Jumlah (Qty)</th>
+                <th style="font-weight: bold; background-color: #3B6653; color: #ffffff; text-align: center; border: 1px solid #000000;">Harga Satuan (Rp)</th>
+                <th style="font-weight: bold; background-color: #3B6653; color: #ffffff; text-align: center; border: 1px solid #000000;">Total Harga (Rp)</th>
+            </tr>
+            @php $totalQty = 0; @endphp
+            @foreach($pengajuan->items as $index => $item)
+            @php $totalQty += $item->qty; @endphp
+            <tr>
+                <td style="text-align: center; border: 1px solid #000000;">{{ $index + 1 }}</td>
+                <td style="border: 1px solid #000000;">{{ $item->nama_barang }}</td>
+                <td style="text-align: center; border: 1px solid #000000;">{{ $item->qty }}</td>
+                <td style="text-align: right; border: 1px solid #000000;">{{ $item->harga_satuan }}</td>
+                <td style="text-align: right; border: 1px solid #000000;">{{ $item->total_harga }}</td>
+            </tr>
+            @endforeach
+            <tr>
+                <td colspan="2" style="text-align: right; font-weight: bold; border: 1px solid #000000;">TOTAL</td>
+                <td style="text-align: center; font-weight: bold; border: 1px solid #000000;">{{ $totalQty }}</td>
+                <td style="border: 1px solid #000000;"></td>
+                <td style="text-align: right; font-weight: bold; border: 1px solid #000000;">{{ $pengajuan->grand_total }}</td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
